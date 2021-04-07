bb8924ab1f23e7f34443c07553766ea618e6ad8b
Jaro <jaro@fakect.tk>
2021-04-08 00:49:33 +0200

    Ok this will fix +mchead

--- skurrol.js
+++ skurrol.js
@@ -321,7 +321,7 @@ bot.command({
     name: "head",
     aliases: ["mc", "kopf", "mchead", "skin"],
     code: `
-    $image[https://mc-heads.net/avatar/$message]
+    $image[https://mc-heads.net/avatar/$message[1].png]
     $color[#ff1c49]
     $footer[If it shows a steve head, then the api doesn't have your head in their database]
     $argsCheck[1;Error 1024, missing, wrong or too many arguments]
