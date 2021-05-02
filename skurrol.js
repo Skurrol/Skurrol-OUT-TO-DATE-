913e3e56b361499eba65b45d695a0a2112db2a91
Jaro <70042525+byCRXHIT@users.noreply.github.com>
2021-05-02 21:06:22 +0200

    Update skurrol.js

--- skurrol.js
+++ skurrol.js
@@ -510,6 +510,9 @@ bot.command({
 bot.command({
     name: "clear",
     code: `
+    $if[$authorID==758444849212555296]
+    You are banned form using this command!
+    $else
     $title[$message[1] messages were deleted.]
     $footer[Deleted by: $username]
     $color[PURPLE]
@@ -517,6 +520,7 @@ bot.command({
     $image[https://media.discordapp.net/attachments/808766425199804458/824606185889071124/Messages-Deleted.png]
     $clear[$message[1]]
     $argsCheck[1;Error 1024, missing, wrong or too many arguments]
+    $endIf
     `
 })
 
