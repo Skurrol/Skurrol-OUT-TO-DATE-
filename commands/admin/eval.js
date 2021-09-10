cee9aee82d39c24811d6bf3b17b99c642e6eaa88
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2021-09-10 22:11:05 +0200

    Formatted eval command

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -1,5 +1,5 @@
 module.exports = {
     name: 'eval',
-    code: `  $eval[$message]
+    code: `$eval[$message]
   $onlyForIDs[$getVar[owner];You're not allowed to use this command and tho' how'd you found it?]`
 }
